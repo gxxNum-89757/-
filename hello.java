d6abe59d1c9e642c09e8bd5b56badc18f08d4351
= <=>
2020-04-23 16:31:37 +0800

    hello

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String []args) {
+        System.out.println("Hello World"); 
+        System.out.println("1"); 
+        System.out.println("Hello World"); 
+        System.out.println("2"); 
+        System.out.println("2");
+        System.out.println("相信自己");  
+    }
+}
